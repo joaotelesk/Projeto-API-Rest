4108bbd830055629ad1624325c80be5c3fb92202
Joao Teles <joaojoseteles@gmail.com>
2022-09-23 19:22:26 -0300

    update README.md

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 mongoose.connect(
-  "mongodb+srv://admin:123@alura.hnrtbyf.mongodb.net/alura-livros"
+  "mongodb+srv://admin:<senha>@alura.hnrtbyf.mongodb.net/alura-livros"
 );
 let db = mongoose.connection;
 export default db;
